9c1f1426c32e6eba26485a8899e735b98fddcc60
DamianoLandi <damianolandi01@gmail.com>
2021-07-21 00:21:31 +0200

    Snack 2

--- blocco 2/script.js
+++ blocco 2/script.js
@@ -28,3 +28,42 @@ for (var key in ball){
 console.log(message);
 
 pallaDisplay.innerHTML = message;
+
+//SNACK 2
+
+var ball2 = {
+    Nome: "palla",
+    Peso: 10,
+}
+
+var pallaDisplay2 = document.getElementById("palla2");
+
+var message2 = "";
+
+for (var key in ball2){
+    message2 += "<li>" + key + ": " + ball2[key] + "</li>";
+}
+
+console.log(message2);
+
+pallaDisplay2.innerHTML = message2;
+
+var enter = document.getElementById("enter");
+
+var numbInput = document.getElementById("number");
+
+enter.addEventListener("click",function(){
+
+    ball2.Peso = numbInput.value;
+
+    var message3 = "";
+
+    for (var key in ball){
+        message3 += "<li>" + key + ": " + ball2[key] + "</li>";
+    }   
+
+    console.log(message3);
+
+    pallaDisplay2.innerHTML = message3;
+
+});
